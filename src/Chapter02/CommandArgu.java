7ff2172465d813cb3dfdcf8e0851e8f19bd6feeb
niushuai <niushuai666@gmail.com>
2015-07-07 13:51:24 +0800

    将 mac 的隐藏文件加入 ignore

--- src/Chapter02/CommandArgu.java
+++ src/Chapter02/CommandArgu.java
@@ -4,14 +4,14 @@ package Chapter02;
  * 
  * @author niushuai
  * 
- * 在Chapter01的上一层目录执行java Chapter01.CommandArgu happy new year
+ *         在Chapter01的上一层目录执行java Chapter01.CommandArgu happy new year
  */
 
 public class CommandArgu {
-	public static void main(String[] args) {
-		System.out.println(args.length);
-		for (int i = 0; i < args.length; ++i) {
-			System.out.println(args[i]);
-		}
-	}
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for (int i = 0; i < args.length; ++i) {
+            System.out.println(args[i]);
+        }
+    }
 }
